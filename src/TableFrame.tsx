5e412bab9bd8e29cf460e1e796f2e50279d2db1b
b-ivanov <bojida.g.i@gmail.com>
2020-09-19 18:39:15 +0300

    Added redux storage

--- src/TableFrame.tsx
+++ src/TableFrame.tsx
@@ -1,34 +1,44 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import MovieRecord from './interfaces'
 import TableColumn from './TableColumn';
-import MovieRecord from './interfaces';
 
-interface MovieRecordProps {
-	moviesList: MovieRecord[];
-}
+class TableFrame extends React.Component {
+	render () {
+		const props:any = this.props;
+		if (props.moviesDB.length === 0) {
+			return (<div>No records to show!</div>);
+		} else {
+			return (
+				<>
+					<table>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Title</th>
+								<th>Director</th>
+								<th>Distributor</th>
+								<th>IMDB rating</th>
+								<th>IMDB votes</th>
+							</tr>
+						</thead>
+						<tbody>
+						{
+							props.moviesDB.map((record: MovieRecord) => {
+								return <TableColumn key={'id_' + record.id} movie={record}/>
+							})
+						}
+						</tbody>
+					</table>
+					<div>Number of records: {props.moviesDB.length}</div>
+				</>
+			);
+		}
+	}
+};
 
-export default function TableFrame({moviesList}: MovieRecordProps) {
-	return (
-		<>
-			<table>
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Title</th>
-						<th>Director</th>
-						<th>Distributor</th>
-						<th>IMDB rating</th>
-						<th>IMDB votes</th>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						moviesList.map(record => {
-							return <TableColumn key={'id_' + record.id} movie={record}/>
-						})
-					}
-				</tbody>
-			</table>
-			<div>Number of records: {moviesList.length}</div>
-		</>
-	)
-}
+const mapStateToProps = (state:any) => ({
+	moviesDB: state.moviesDB
+})
+
+export default connect(mapStateToProps)(TableFrame);
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './App.css';
+import moviesJSON from './movies.json';
+import TableFrame from './TableFrame';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('moviesTable')
+const initialState = {
+  moviesDB: moviesJSON
+};
+
+const reducer = (state:any = initialState, action:any) => {
+	return state;
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+	<Provider store={store}>
+		<TableFrame />
+	</Provider>
 );
+
+ReactDOM.render(<App />, document.getElementById("moviesTable"));
